4dc5f2bbab1048750c4bb54b94af44e03fb8d195
Rana Amr Afifi <rana.amr.afifi@gmail.com>
2017-07-30 14:11:33 +0200

    Update HassanSuggestion.go
    Update init-putstate

--- Trial/HassanSuggestion.go
+++ Trial/HassanSuggestion.go
@@ -54,11 +54,15 @@ func main() {
 // Init resets all the things
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+	if len(args) != 3 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 3")
 	}
-
-	err := stub.PutState("hello_world", []byte(args[0]))
+        
+	trans:=Transaction{Username:args[0], ItemName:args[1], QRCode: args[2]}
+	
+        transactionBytes, err := json.Marshal(&trans)
+	// Missing Check here 
+	err := stub.PutState(args[2],  transactionBytes) 
 	if err != nil {
 		return nil, err
 	}
