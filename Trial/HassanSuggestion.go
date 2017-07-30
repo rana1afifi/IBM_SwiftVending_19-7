c83d49dac8ebf489094a452ab3c16820d7d11fe8
Rana Amr Afifi <rana.amr.afifi@gmail.com>
2017-07-30 14:44:26 +0200

    Update HassanSuggestion.go

--- Trial/HassanSuggestion.go
+++ Trial/HassanSuggestion.go
@@ -56,7 +56,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	
         transactionBytes, err := json.Marshal(&trans)
 	// Missing Check here 
-	err2 := stub.PutState(args[2] ,transactionBytes) 
+	err = stub.PutState(args[2] ,transactionBytes) 
 	if err != nil {
 		return nil, err
 	}
